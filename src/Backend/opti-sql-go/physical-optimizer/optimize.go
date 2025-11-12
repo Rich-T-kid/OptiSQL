25aea9dd95005ebfd5741024cf4fe24826260dde
RIchard Baah <137434454+Rich-T-kid@users.noreply.github.com>
2025-11-11 22:50:52 -0500

    Added lots of configuration and documentation to start the project (#6)
    Co-authored-by: Richard Baah <richardbaah@MacBook-Air-de-Richard.local>

--- src/Backend/opti-sql-go/physical-optimizer/optimize.go
+++ src/Backend/opti-sql-go/physical-optimizer/optimize.go
@@ -1,1 +1,3 @@
 package physicaloptimizer
+
+// optimize the parsed plan
--- src/Backend/opti-sql-rs/src/main.rs
+++ src/Backend/opti-sql-rs/src/main.rs
@@ -1,8 +1,8 @@
-use datafusion::arrow::record_batch::RecordBatch;
+//use datafusion::arrow::record_batch::RecordBatch;
 mod project;
-fn main(){
+#[allow(dead_code)]
+fn main() {
     println!("Hello, world!");
     project::project_exec::project_execute();
     project::source::csv::read_csv();
-    
 }
