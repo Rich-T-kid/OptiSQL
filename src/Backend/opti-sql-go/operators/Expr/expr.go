f97e73b437378f5710edcb48e3f81c5280bf5f67
Richard Baah <richardbaah@MacBook-Air-de-Richard.local>
2025-11-17 02:17:45 -0500

    chore: add next steps. Looking at Expr and Filter operators

--- src/Backend/opti-sql-go/operators/Expr/expr.go
+++ src/Backend/opti-sql-go/operators/Expr/expr.go
@@ -4,3 +4,21 @@ package Expr
 // for example Column + Literal
 // Column - Column
 // Literal / Literal
+
+//1. Arithmetic Expressions
+// SELECT salary * 1.2, price + tax, -(discount)
+//2.Alias Expressions
+//SELECT name AS employee_name, age AS employee_age
+//SELECT salary * 1.2 AS new_salary
+//3.String Expressions
+//first_name || ' ' || last_name
+//UPPER(name)
+//LOWER(email)
+//SUBSTRING(name, 1, 3)
+//4. Function calls
+//ABS(x)
+//ROUND(salary, 2)
+//LENGTH(name)
+//COALESCE(a, b)
+//5. Constants
+//SELECT 1, 'hello', 3.14
