ee7d4a2a77090c889d72b7e4304a3ada905c926d
Richard Baah <richardbaah@MacBook-Air-de-Richard.local>
2025-11-18 00:45:35 -0500

    define Expr Interface & Expr Operators

--- src/Backend/opti-sql-go/operators/Expr/expr.go
+++ src/Backend/opti-sql-go/operators/Expr/expr.go
@@ -1,24 +1,105 @@
 package Expr
 
-// evaluate expressions
-// for example Column + Literal
-// Column - Column
-// Literal / Literal
+import "github.com/apache/arrow/go/v17/arrow"
 
-//1. Arithmetic Expressions
-// SELECT salary * 1.2, price + tax, -(discount)
-//2.Alias Expressions
-//SELECT name AS employee_name, age AS employee_age
-//SELECT salary * 1.2 AS new_salary
-//3.String Expressions
-//first_name || ' ' || last_name
-//UPPER(name)
-//LOWER(email)
-//SUBSTRING(name, 1, 3)
-//4. Function calls
-//ABS(x)
-//ROUND(salary, 2)
-//LENGTH(name)
-//COALESCE(a, b)
-//5. Constants
-//SELECT 1, 'hello', 3.14
+type binaryOperator int
+
+const (
+	// arithmetic
+	addition       binaryOperator = 1
+	subtraction    binaryOperator = 2
+	multiplication binaryOperator = 3
+	division       binaryOperator = 4
+	modulous       binaryOperator = 5
+	// comparison
+	equal              binaryOperator = 6
+	notEqual           binaryOperator = 7
+	lessThan           binaryOperator = 8
+	lessThanOrEqual    binaryOperator = 9
+	greaterThan        binaryOperator = 10
+	greaterThanOrEqual binaryOperator = 11
+	// logical
+	and binaryOperator = 12
+	or  binaryOperator = 13
+	not binaryOperator = 14
+)
+
+type supportedFunctions int
+
+const (
+	upper supportedFunctions = 1
+	lower supportedFunctions = 2
+	abs   supportedFunctions = 3
+	round supportedFunctions = 4
+)
+
+type aggFunctions = int
+
+const (
+	Sum   aggFunctions = 1
+	Count aggFunctions = 2
+	Avg   aggFunctions = 3
+	Min   aggFunctions = 4
+	Max   aggFunctions = 5
+)
+
+/*
+Eval(expr):
+
+	match expr:
+	    Literal(x) -> return x
+	    Column(name) -> return array of that column
+	    BinaryExpr(left > right) -> eval left, eval right, apply operator
+	    ScalarFunction(upper(name)) -> evaluate function
+	    Alias(expr, name) -> just a name wrapper
+*/
+type Expr interface {
+	ExprNode() // empty method, only for the sake of polymophism
+}
+
+/*
+Alias | sql: select col1 as new_name from table_source
+updates the column name in the output schema.
+*/
+type Alias struct {
+	expr       []Expr
+	columnName string
+	name       string
+}
+
+// return batch.Columns[fieldIndex["age"]]
+// resolves the arrow array corresponding to name passed in
+// sql: select age
+type ColumnResolve struct {
+	name string
+}
+
+// Evaluates to a column of length = batch-size, filled with this literal.
+// sql: select 1
+type LiteralResolve struct {
+	Type  arrow.DataType
+	value any
+}
+
+type Operator struct {
+	o binaryOperator
+}
+type BinaryExpr struct {
+	left  []Expr
+	op    Operator
+	right []Expr
+}
+
+type ScalarFunction struct {
+	function supportedFunctions
+	input    []Expr // resolve to something you can procees IE, literal/coloumn Resolve
+}
+type AggregateFunction struct {
+	function aggFunctions
+	args     []Expr
+}
+
+type CastExpr struct {
+	expr       []Expr // can be a Literal or Column (check for datatype then)
+	targetType arrow.DataType
+}
