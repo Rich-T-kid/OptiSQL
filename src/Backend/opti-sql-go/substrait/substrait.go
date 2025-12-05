0abb34430273b33e40da1508409a242c4ec5deab
Richard Baah <richardbaah@MacBook-Air-de-Richard.local>
2025-12-05 02:53:00 -0500

    feat: generate test substrait plans & basic parse

--- src/Backend/opti-sql-go/substrait/substrait.go
+++ src/Backend/opti-sql-go/substrait/substrait.go
@@ -1,1 +1,7 @@
 package substrait
+
+// turn raw bytes to substrait plan
+// parse substrait plan to in memory format we can work with (abstract syntax tree)
+// optimize the plan (apply optimizations)
+// run operators based on the optimized plan
+//
